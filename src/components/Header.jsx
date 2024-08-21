29db0bb65ea6373cc1c56fd52f928a500a8d16cb
Дмитрий Тульников <tulnikov@MacBookPro.local>
2024-08-21 22:14:02 +0300

    Init

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import styles from './style.module.css'
+
+function Header() {
+    return <div className={styles.header}>
+        <div>Баланс</div>
+        <div>👨‍🦱</div>
+    </div>
+}
+
+export default Header
