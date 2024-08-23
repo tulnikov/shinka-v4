b972ed54239b49a1923979d380c7623effdd9fcc
tulnikov <dima@tulnikov.ru>
2024-08-23 09:52:16 +0300

    offiice

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
-import Header from './components/Header'
-import Content from './components/Content'
-import Footer from './components/Footer'
+
 
 function App() {
 
     return (
         <>
-            <Header/>
-            <Content/>
-            <Footer/>
+
         </>
     )
 }
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.css';
-
-function Footer() {
-    return <footer className={styles.footer}>
-        <div>2024</div>
-    </footer>
-}
-
-export default Footer;
